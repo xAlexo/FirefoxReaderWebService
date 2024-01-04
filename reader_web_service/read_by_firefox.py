fa251d4f407ea2e493c9888b4b30341f756c35ef
Alexo <alexo@live.ru>
2024-01-04 23:15:11 +0300

    fix: getting content

--- reader_web_service/read_by_firefox.py
+++ reader_web_service/read_by_firefox.py
@@ -23,14 +23,18 @@ def read_by_firefox(url):
 
     try:
         WebDriverWait(browser, 30).until(
-            ec.presence_of_element_located((By.ID, 'post-content-body'))
+            ec.presence_of_element_located((By.ID, 'article'))
         )
         return {
             'title': browser.find_element(
-                By.CLASS, 'reader-title').get_attribute('innerHTML'),
+                By.CLASS_NAME, 'reader-title').get_attribute('innerHTML'),
             'content': browser.find_element(
-                By.ID, 'article').get_attribute('innerHTML')
+                By.ID, 'article').get_attribute('innerHTML').strip(),
         }
     finally:
         browser.quit()
         display.stop()
+
+
+if __name__ == '__main__':
+    print(read_by_firefox('https://www.mirf.ru/serial/harli-kvinn-3-j-sezon-kak-lego-betmen-no-bez-lego/'))
