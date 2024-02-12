be95cf8b80c21f9f74938950b107810d9f5e672b
Alexo <alexo@live.ru>
2024-02-12 14:23:07 +0300

    fix: find reader page content

--- reader_web_service/read_by_firefox.py
+++ reader_web_service/read_by_firefox.py
@@ -23,31 +23,26 @@ def read_by_firefox(url, reader=True):
     browser.get(url)
 
     try:
+        WebDriverWait(browser, 20).until(
+            ec.presence_of_element_located((By.CLASS_NAME, 'page'))
+        )
         if reader:
-            try:
-                WebDriverWait(browser, 20).until(
-                    ec.presence_of_element_located((By.CLASS_NAME, 'reader-title'))
-                )
-            except Exception as e:
-                _log.debug('Reader not found')
-                _log.debug(f'{e}')
-                _log.debug(f'{browser.page_source}')
-
-                return
-
             return {
                 'title': browser.find_element(
                     By.CLASS_NAME, 'reader-title').get_attribute('innerHTML'),
                 'content': browser.find_element(
-                    By.CLASS_NAME, 'content').get_attribute('innerHTML').strip(),
+                    By.CLASS_NAME, 'page').get_attribute('innerHTML').strip(),
             }
-
         return {
             'title': browser.find_element(
                 By.TAG_NAME, 'title').get_attribute('innerHTML'),
             'content': browser.find_element(
                 By.TAG_NAME, 'body').get_attribute('innerHTML').strip(),
         }
+    except Exception as e:
+        _log.debug('Reader not found')
+        _log.debug(f'{e}')
+        _log.debug(f'{browser.page_source}')
     finally:
         browser.quit()
         display.stop()
