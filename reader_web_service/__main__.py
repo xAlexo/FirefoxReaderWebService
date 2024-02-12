7d490187d15cd0084b851254478ded5f29c9d6a8
Alexo <alexo@live.ru>
2024-02-12 17:06:20 +0300

    fix: one timeout

--- reader_web_service/__main__.py
+++ reader_web_service/__main__.py
@@ -15,19 +15,15 @@ app = FastAPI()
 @app.get("/")
 async def root(url, ):
     loop = asyncio.get_running_loop()
-
-    data = None
-    async with timeout(25):
-        data = await loop.run_in_executor(None, read_by_firefox, url)
-        data_debug = dumps(
-            data, indent=4, ensure_ascii=False, sort_keys=True, default=str)
-        _log.debug(f'data: {data_debug}')
-
+    data = await loop.run_in_executor(None, read_by_firefox, url)
     if not data:
         return JSONResponse(content=jsonable_encoder({
             "error": "reader not found",
         }), status_code=400)
 
+    data_debug = dumps(
+        data, indent=4, ensure_ascii=False, sort_keys=True, default=str)
+    _log.debug(f'data: {data_debug}')
     return JSONResponse(content=jsonable_encoder({
         "title": data['title'],
         "html": data['content'],
@@ -37,11 +33,7 @@ async def root(url, ):
 @app.get("/html")
 async def root(url, ):
     loop = asyncio.get_running_loop()
-
-    data = None
-    async with timeout(25):
-        data = await loop.run_in_executor(None, read_by_firefox, url, False)
-
+    data = await loop.run_in_executor(None, read_by_firefox, url, False)
     if not data:
         return JSONResponse(content=jsonable_encoder({
             "error": "reader not found",
